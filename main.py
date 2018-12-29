382f93991ee2c5518083f254af02468b71364437
SimonRacaud <simonracaudgmail.com>
2018-12-29 10:07:06 +0100

    Amélioration des commentaires

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+"""
+	SPACETHON an AstroPI Project
+		2018-2019
+	
+	Authors: GAZEAU Lucas, RACAUD Simon, POIRIER Victor, MASSE Thomas 
+
+	---
+
+	Note: 	get_sensors_value(), write_result()			: A compléter
+			is_night(), draw_area(), analyse_picture()	: A faire
+"""
+
 import os
 import time
 
@@ -12,27 +24,31 @@ from picamera import PiCamera
 
 
 """ 
-		Global variables
+		Global variables/constants
 """
-# CONSTANTS
+### CONSTANTS
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 TIME_LOOP = 000 # duration of a loop
 TIME_STOP = 000 # duration from which the program stop
 
+### VARIABLES
 loop = True
 nbImage = 1
 start_program_time = int(time.time()) # timestamp when the program has started (in second)
 start_loop_time = 0 # timestamp when the loop has started
 
-# CAMERA
+""" 
+		Config peripherals
+"""
+### CAMERA
 camera = PiCamera()
 camera.resolution = (640,480)
 
-# SENSEHAT
+### SENSEHAT
 sh = SenseHat()
 sense.set_imu_config(True, False, True) # (compass_enabled, gyro_enabled, accel_enabled)
 
-# 	Matrix led
+##	Matrix led
 # 		Colors
 g = [0,50,0]
 o = [0,0,0]
@@ -48,12 +64,12 @@ img1 = [
 	g,g,g,g,g,g,g,g,
 ]
 
-# Logfile parameters
+### Logfile parameters
 """[a voir]""" formatter = logging.Formatter('%(timestamp)s - %(water)s - %(ground)s - %(cloud)s - %(other)s - %(magnetometer)s - %(accelerometer)s') # Set a custom formatter
 logzero.formatter(formatter)
 logzero.logfile(DIR_PATH+"/data01.csv") # Create the CSV file
 
-# Config ephem
+### Config ephem
 name = "ISS (ZARYA)"
 line1 = "1 25544U 98067A   18362.36563081 -.00011736  00000-0 -17302-3 0  9998"
 line2 = "2 25544  51.6377 131.0297 0002747 209.2490 215.4464 15.53700691148714"
@@ -62,10 +78,12 @@ iss = ephem.readtle(name, line1, line2)
 # utilisation : print(iss.sublat, iss.sublong)
 iss.compute()
 
-####
+""" 
+		[FUNCTIONS]
+"""
 
 def update_matrix(state):
-	""" Update the state of the matrix led """
+	""" Update the state of the matrix led. """
 	if state == 0:
 		sh.set_pixels(img1)
 	elif state == 1:
@@ -82,20 +100,24 @@ def get_loop_timestamp():
 	start_loop_time = time.time()
 
 def get_sensors_value():
-	""" Get the value of the sensors """
+	""" Get the value of the sensors 
+	"""
 	magnetometer = sense.get_compass_raw() # magnetometer
 	accelerometer = sense.get_accelerometer() # accelerometer
 	return (magnetometer, accelerometer)
 
 def take_picture():
-	""" Prend une photo et l'enregistre dans un fichier """
+	""" Prend une photographie et l'enregistre dans un nouveau fichier. 
+	"""
 	get_latlon() # Set lat/long data in the meta data of the picture
 	camera.capture(dir_path+"/image_"+str(nbImage).zfill(3)+".jpg")
 	iss.compute() # mise à jour coordonnés
 	nbImage += 1
 
 def get_latitude_longitude():
-	""" Get latitude/longitude and set there in the meta data of the camera """
+	""" Get latitude/longitude and set there in the meta data of the camera 
+		Récupérer la position en longitude et latitude de l'ISS afin de l'écrire dans les métadonnées des prises de vues.
+	"""
     iss.compute() # Get the lat/long values from ephem
 
     long_value = [float(i) for i in str(iss.sublong).split(":")]
@@ -124,12 +146,19 @@ def get_latitude_longitude():
 """
 
 def is_night():
+	""" Détecter si la luminosité d'une image est suffisante. """
 	pass
 
 def draw_area():
+	""" Traiter l'image prise par l'astro pi afin d'identifier les sufaces de terre, d'eau et de nuage.
+
+	"""
 	pass
 
 def analyse_picture():
+	""" Récupérer le % d'eau, de terre, de nuage et de surface x sur l'image traité par draw_area().
+
+	"""
 	pass
 
 """
@@ -137,12 +166,15 @@ def analyse_picture():
 """
 
 def write_result(timestamp, water, ground, cloud, other, magnetometer, accelerometer):
-	""" Write data in the CSV file """
-	# Save the data to the file
+	""" Write data in the CSV file 
+		Ecrire les données récupéré durant la boucle sur une nouvelle ligne du fichier CSV.
+	"""	
 	logger.info("%s,%s,%s,%s,%s", timestamp, water, )
 
 def check_time():
-	""" check if the program must be stopped """
+	""" Check if the program must be stopped 
+		Si le temps d'execution à dépassé la limite : arreter le programme
+	"""
 	now = int(time.time()) # in second
 	# 3h == 10,800 seconds
 	if (now - start_time) >= TIME_STOP:
@@ -152,7 +184,10 @@ def check_time():
 		wait()
 
 def wait():
-	""" Wait before start again  """
+	""" Wait before start again  
+		Call by : check_time()
+		Attendre que la durée TIME_LOOP se soit écoulé avant de commencer la prochaine boucle.
+	"""
 	now = time.time()
 	if (now - start_loop_time) < TIME_LOOP:
 		time.sleep(TIME_LOOP - (now - start_loop_time))
